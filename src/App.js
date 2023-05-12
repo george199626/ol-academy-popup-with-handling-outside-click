bca786f4db497aed40c5ff96342f988d38400ba5
Giorgi Giorgadze <112405980+george199626@users.noreply.github.com>
2023-05-13 01:53:11 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Popup } from './components/Popup';
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+    
+      <Popup/>
+
+       
+      </div>
+    );
+  }
+}
+
+export default App;
